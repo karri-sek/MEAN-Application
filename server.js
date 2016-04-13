0b7f7988af940098f7aece82ab3bb5d72389874e
sekhar karri <babusek@gmail.com>
2016-04-13 12:49:28 +0100

    Adding morgan a logger and body parser and css preprocessor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     stylus  = require('stylus'),
-    logger  = require('morgan');
+    logger  = require('morgan'),
+    bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV  = process.env.NODE_ENV  || 'development';
 
@@ -13,6 +14,10 @@ function compile(str,path){
 app.set('views',__dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.use(stylus.middleware(
   {
   	 src: __dirname + '/public',
